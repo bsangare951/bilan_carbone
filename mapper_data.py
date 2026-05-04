79e81a3144a5d156a7ac4ebb4247e9b9147ba6be
bsangare951 <bsangare951@github.com>
2026-05-04 12:00:39 +0200

    programmation des dictionnaires de données

--- mapper_data.py
+++ mapper_data.py
@@ -1,3 +1,30 @@
 import load_data as ld
 import process_data as pt
 
+def get_units_referential():
+    return {
+        "ENERGIE": ["kWh", "MWh", "m3", "L"],
+        "TRANSPORT": ["km", "L", "passager.km", "t.km"],
+        "DECHETS": ["kg", "t"],
+        "IMMOBILISATION": ["m2", "unite", "kg"],
+        "AUTRES": ["€", "k€"]
+    }
+
+def get_vehicle_attributes():
+    return ["type", "marque", "modèle", "année", "carburant", "puissance_fiscale"]
+
+def get_carbon_schema():
+    return {
+        "Date": None,
+        "ID_Facture": None,
+        "Designation": None, 
+        "Prix": 0.0,
+        "Quantite": 0.0,
+        "Unite": None,
+        "Details_Vehicule": {
+            "Marque_Modele": None,
+            "Carburant": None,
+            "Type": None # VP ou VUL
+        },
+        "Emissions_tCO2e": 0.0
+    }
